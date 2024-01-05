94d7308ad8f36eb523656a914749849f26eccbac
frankgrimy <f.gonzalez.12@ug.uchile.cl>
2024-01-05 15:13:56 -0300

    Added toggle buttons and their painting method

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,40 @@ MidSiderAudioProcessorEditor::MidSiderAudioProcessorEditor (MidSiderAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (250, 85);
+    setResizable(true, false);
+    getConstrainer()->setFixedAspectRatio(2.941176470588235);
+    setResizeLimits(250, 85, 500, 170);
+
+    addAndMakeVisible(&stereo);
+    stereo.setButtonText("Stereo");
+    stereo.onClick = [this] {
+      audioProcessor.setStereoMidSideIndex(0);
+      };
+
+    addAndMakeVisible(&mid);
+    mid.setButtonText("Mid");
+    mid.onClick = [this] {
+      audioProcessor.setStereoMidSideIndex(1);
+      };
+
+    addAndMakeVisible(&side);
+    side.setButtonText("Side");
+    side.onClick = [this] {
+      audioProcessor.setStereoMidSideIndex(2);
+      };
+
+    addAndMakeVisible(&left2Mono);
+    left2Mono.setButtonText("L->Mono");
+    left2Mono.onClick = [this] {
+      audioProcessor.setStereoMidSideIndex(3);
+      };
+
+    addAndMakeVisible(&right2Mono);
+    right2Mono.setButtonText("R->Mono");
+    right2Mono.onClick = [this] {
+      audioProcessor.setStereoMidSideIndex(4);
+      };
 }
 
 MidSiderAudioProcessorEditor::~MidSiderAudioProcessorEditor()
@@ -29,12 +62,37 @@ void MidSiderAudioProcessorEditor::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("MidSider\nby Frank Grimy", getLocalBounds(), juce::Justification::centred, 1);
+    //g.setFont (15.0f);
+    g.setFont(juce::Font("Calibri", getWidth() * 0.048f, juce::Font::bold));
+    g.drawFittedText ("MidSider\nby Frank Grimy", getLocalBounds(), juce::Justification::centredBottom, 1);
+
+    buttonPaint();
+
 }
 
 void MidSiderAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    int windowX = getWidth();
+    int windowY = getHeight();
+
+    int buttonSize = windowX/5;
+
+    stereo.setSize(buttonSize, buttonSize);
+    stereo.setTopLeftPosition(0, 0);
+
+    mid.setSize(buttonSize, buttonSize);
+    mid.setTopLeftPosition(stereo.getRight(), 0);
+
+    side.setSize(buttonSize, buttonSize);
+    side.setTopLeftPosition(mid.getRight(), 0);
+
+    left2Mono.setSize(buttonSize, buttonSize);
+    left2Mono.setTopLeftPosition(side.getRight(), 0);
+
+    right2Mono.setSize(buttonSize, buttonSize);
+    right2Mono.setTopLeftPosition(left2Mono.getRight(), 0);
+
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -24,10 +24,55 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void buttonPaint(){
+    if (audioProcessor.getStereoMidSideIndex() == 0)
+    {
+      stereo.setToggleState(true, juce::dontSendNotification);
+      mid.setToggleState(false, juce::dontSendNotification);
+      side.setToggleState(false, juce::dontSendNotification);
+      left2Mono.setToggleState(false, juce::dontSendNotification);
+      right2Mono.setToggleState(false, juce::dontSendNotification);
+    }
+    else if (audioProcessor.getStereoMidSideIndex() == 1)
+    {
+      stereo.setToggleState(false, juce::dontSendNotification);
+      mid.setToggleState(true, juce::dontSendNotification);
+      side.setToggleState(false, juce::dontSendNotification);
+      left2Mono.setToggleState(false, juce::dontSendNotification);
+      right2Mono.setToggleState(false, juce::dontSendNotification);
+    }
+    else if (audioProcessor.getStereoMidSideIndex() == 2)
+    {
+      stereo.setToggleState(false, juce::dontSendNotification);
+      mid.setToggleState(false, juce::dontSendNotification);
+      side.setToggleState(true, juce::dontSendNotification);
+      left2Mono.setToggleState(false, juce::dontSendNotification);
+      right2Mono.setToggleState(false, juce::dontSendNotification);
+    }
+    else if (audioProcessor.getStereoMidSideIndex() == 3)
+    {
+      stereo.setToggleState(false, juce::dontSendNotification);
+      mid.setToggleState(false, juce::dontSendNotification);
+      side.setToggleState(false, juce::dontSendNotification);
+      left2Mono.setToggleState(true, juce::dontSendNotification);
+      right2Mono.setToggleState(false, juce::dontSendNotification);
+    }
+    else if (audioProcessor.getStereoMidSideIndex() == 4)
+    {
+      stereo.setToggleState(false, juce::dontSendNotification);
+      mid.setToggleState(false, juce::dontSendNotification);
+      side.setToggleState(false, juce::dontSendNotification);
+      left2Mono.setToggleState(false, juce::dontSendNotification);
+      right2Mono.setToggleState(true, juce::dontSendNotification);
+    };
+    }
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MidSiderAudioProcessor& audioProcessor;
+
+    juce::TextButton stereo, mid, side, left2Mono, right2Mono;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MidSiderAudioProcessorEditor)
 };
